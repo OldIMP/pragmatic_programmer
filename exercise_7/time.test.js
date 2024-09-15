42f06b1db6d94011ac410464e297b8a5992e8c6c
LIU Shouhai <shouhai.liu@extern.brz.gv.at>
2024-09-15 21:10:34 +0200

    Parameterize tests.

--- exercise_7/time.test.js
+++ exercise_7/time.test.js
@@ -1,19 +1,21 @@
 let parser = require('./time.js');
 
-function testTime(input, expectedHour, extractedMin = 0) {
-    test('parse ' + input, () => expect(parser.parse(input)).toEqual(expectedHour * 60 + extractedMin));
-}
+describe("time.js can parse", () => {
+    it.each([
+        ['4pm', 16],
+        ['7:38pm', 19, 38],
+        ['23:42', 23, 42],
+        ['3:16', 3, 16],
+        ['3:16am', 3, 16],
 
-testTime('4pm', 16);
-testTime('7:38pm', 19, 38);
-testTime('23:42', 23, 42);
-testTime('3:16', 3, 16);
-testTime('3:16am', 3, 16);
-
-testTime('1am', 1);
-testTime('1pm', 13);
-testTime('2:30', 2, 30);
-testTime('14:30', 14, 30);
-testTime('2:30pm', 14, 30);
+        ['1am', 1],
+        ['1pm', 13],
+        ['2:30', 2, 30],
+        ['14:30', 14, 30],
+        ['2:30pm', 14, 30],
+    ])
+    ("when parsing '%s'", (input, expectedHour, extractedMin = 0) =>
+        expect(parser.parse(input)).toEqual(expectedHour * 60 + extractedMin));
+});
 
 test('num is invalid', () => expect(() => parser.parse('1')).toThrow(parser.SyntaxError));
