7a866c153fa53c1003ded0b3a89add62ed6766bb
LIU Shouhai <shouhai.liu@extern.brz.gv.at>
2024-09-15 21:29:18 +0200

    Add more validation to exercise 7.

--- exercise_7/time.test.js
+++ exercise_7/time.test.js
@@ -1,13 +1,14 @@
 let parser = require('./time.js');
 
-describe("time.js can parse", () => {
+describe("time.js can parse", () =>
     it.each([
+        // from Exercise 7
         ['4pm', 16],
         ['7:38pm', 19, 38],
         ['23:42', 23, 42],
         ['3:16', 3, 16],
         ['3:16am', 3, 16],
-
+        // from Answer 7
         ['1am', 1],
         ['1pm', 13],
         ['2:30', 2, 30],
@@ -15,7 +16,8 @@ describe("time.js can parse", () => {
         ['2:30pm', 14, 30],
     ])
     ("when parsing '%s'", (input, expectedHour, extractedMin = 0) =>
-        expect(parser.parse(input)).toEqual(expectedHour * 60 + extractedMin));
-});
+        expect(parser.parse(input)).toEqual(expectedHour * 60 + extractedMin)));
 
-test('num is invalid', () => expect(() => parser.parse('1')).toThrow(parser.SyntaxError));
+describe("time.js can't parse", () =>
+    it.each(['1', ':30pm', '13:24pm', '1:60', '13pm', '01:60am'])
+    ("when parsing '%s'", input => expect(() => parser.parse(input)).toThrow(parser.SyntaxError)));
